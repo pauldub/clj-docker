02a2bca93c32df67e7757b391da1eb4b1402f33d
U-Paul-PC\Paul <Paul@Paul-PC.(none)>
2013-10-12 18:07:16 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Docker
+
+A work in progress Clojure library designed to use the Docker http API.
+
+## TODO
+
+- Implement all APIs.
+- Write tests.
+- Write doc.
+
+## Usage
+
+```
+(require ['docker.core :as 'docker])
+(docker/info)
+```
---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject docker "0.1.0-SNAPSHOT"
+  :description "A work in progress docket API client"
+  :url "http://endou.fr/clojure/docker"
+  :license {:name "MIT License"
+            :url "http://choosealicense.com/licenses/mit"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.apache.commons/commons-compress "1.4.1"]
+                 [http-kit "2.1.5"]
+                 [aleph "0.3.0-rc2"]
+                 [cheshire "5.2.0"]])
