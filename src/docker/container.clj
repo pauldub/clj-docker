2560624e88027ba0e389aea87cd0f019f3db614c
Timo Sulg <timgluz@gmail.com>
2014-03-25 19:01:38 +0200

    additional methods for container;

--- src/docker/container.clj
+++ src/docker/container.clj
@@ -2,7 +2,12 @@
   (:require [docker.client :as dc]
             [slingshot.slingshot :refer [throw+ try+]]
             [taoensso.timbre :as log]
+            [cheshire.core :refer [generate-string]]
             [clojure.java.io :as io]))
+
+;;TODO: experiment response-handlers as multimethods not select-case
+;; defmultu container-response-handler [status body error] ...
+;;
 
 (def exceptions {:server-error {:type ::server_error
                                 :status 500
@@ -12,7 +17,13 @@
                                :message "Unspecified error: timeout or bug in source."}
                  :bad-parameter {:type ::bad_parameter
                                  :status 400
-                                 :message "Bad parameter - check arguments."}})
+                                 :message "Bad parameter - check arguments."}
+                 :no-container {:type ::no_container
+                                :status 404
+                                :message "Container doesnt exists"}
+                 :not-attached {:type ::not_attached
+                                :status 406
+                                :message "Container not running - cant attache."}})
 
 
 (defn show-all
@@ -43,3 +54,113 @@
       200 (dc/parse-json body)
       400 (throw+ (:bad-parameter exceptions))
       (throw+ (:unspecified exceptions)))))
+
+(def default-container-config
+  {:Hostname ""
+   :User ""
+   :Memory 0
+   :MemorySwap 0
+   :AttachStdin false
+   :AttachStdout true
+   :AttachSterr true
+   :PortSpecs nil
+   :Tty false
+   :OpenStdin false
+   :StdinOnce false
+   :Env nil
+   :Cmd ["date"]
+   :Dns nil
+   :Image "base"
+   :Volumes {"/tmp" {}}
+   :VolumesFrom ""
+   :WorkingDir ""
+   :ExposedPorts {"22/tcp" {}}})
+
+;;TODO:add validators
+(defn create-config
+  [user-configs]
+    (merge default-container-config user-configs))
+
+
+(defn create
+  "creates new container
+  Arguments:
+    client          - initialized dockers client
+    container-name  - a name of container (only alphanum & _)
+    user-configs    - map of configurations user wants to change on default-container-config (optional)
+  Usage:
+    (create client \"conta\")
+    (create client \"conta2\" {:Hostname \"conta2\", :Memory 1024})"
+  ([client container-name] (create client container-name {}))
+  ([client container-name user-configs]
+    (let [request-data (create-config user-configs)
+          {:keys [status body]} (dc/rpc-post client "/containers/create"
+                                             {:query-params {:name container-name}
+                                              :body (generate-string request-data)})]
+      (case status
+        201 (dc/parse-json body)
+        404 (throw+ (:no-container exceptions))
+        406 (throw+ (:not-attached exceptions))
+        500 (throw+ (:server-error exceptions))
+        (throw+ (:unspecified exceptions))))))
+
+(defn inspect
+  "returns low-level information on the container id
+  Arguments:
+    client        - the initialized docker's client
+    container-id  - the id of the container
+  Returns:
+    a clojure map
+  Usage:
+    (inspect client \"aabcdef1234\")"
+
+  [client container-id]
+  (let [{:keys [status body]} (dc/rpc-get client
+                                          (str "/containers/" container-id  "/json"))]
+    (case status
+      200 (dc/parse-json body)
+      404 (throw+ (:no-container exceptions))
+      500 (throw+ (:server-error exceptions))
+      (throw+ (:unspecified exceptions)))))
+
+(defn top
+  "lists processes running inside the container id.
+  Arguments:
+    client        - a initialized docker's client
+    container-id  - the id of the container
+  Optional arguments:
+    :ps_args - ps arguments to use (e.g 'aux')
+  Usage:
+    (top client \"abse123\")"
+  [client container-id & {:keys [ps_args]}]
+  (let [{:keys [status body]} (dc/rpc-get client
+                                          (str "/containers/" container-id "/top")
+                                          {:query-params {:ps_args ps_args}})]
+    (case status
+      200 (dc/parse-json body)
+      404 (throw+ (:no-container exceptions))
+      500 (throw+ (:server-error exceptions))
+      (throw+ (:unspecified exceptions)))))
+
+(defn changes
+  "shows changes on container id's filesystem
+  Arguments:
+    client        - a initialized client
+    container-id  - the id of the container
+  Usage:
+    (changes client \"abc123\")"
+  [client container-id]
+  (let [{:keys [status body]} (dc/rpc-get client
+                                          (str "/containers/" container-id "/changes"))]
+    (case status
+      200 (dc/parse-json body)
+      404 (throw+ (:no-container exceptions))
+      500 (throw+ (:server-error exceptions))
+      (throw+ (:unspecified exceptions)))))
+
+;;TODO: continue here
+(defn export
+  ""
+  [client])
+
+
--- test/docker/mocked_container.clj
+++ test/docker/mocked_container.clj
@@ -8,21 +8,103 @@
             [docker.client :refer [make-client]]
             [docker.container :refer :all]))
 
+;;TODO: check exception content & type
+
 (def default-host "http://10.0.1.2:4243")
 
 (facts "show-all"
   (let [client (make-client default-host)
         con1 {:Id "abc1", :Image "test:latest"}
         con2 {:Id "abc2", :Image "test:12334"}]
-    (facts "shows correct containers with default params"
+    (fact "shows correct containers with default params"
       (with-fake-http [#"/containers/json" (generate-string [con1 con2])]
         (let [containers (show-all client)]
           (count containers) => 2
           (first containers) => con1
           (second containers) => con2)))
-    (facts "shows only 1 and latest container"
+    (fact "shows only 1 and latest container"
       (with-fake-http [#"/containers/json" (generate-string [con1])]
         (first (show-all client :limit 1)) => con1))
-    (facts "raises exceptions when using not valid value for param."
+    (fact "raises exceptions when using not valid value for param."
       (with-fake-http [#"/containers/json" {:status 400}]
         (show-all client :since 1001) => (throws Exception)))))
+
+
+(facts "create new container"
+  (let [client (make-client default-host)
+        resp1 {:Id "e90d80", :Warnings []}]
+    (fact "works with default params"
+      (with-fake-http [#"/containers/create" {:status 201
+                                              :body (generate-string resp1)}]
+        (create client "container1") => resp1))
+    (fact "works with user configration"
+      (with-fake-http [#"/containers/create" {:status 201
+                                              :body (generate-string resp1)}]
+        (create client "container1" {:Memory 0 :Cmd ["echo 1"]}) => resp1))
+    (fact "raises exception when container doesnt exists"
+      (with-fake-http [#"/containers/create" {:status 404}]
+        (create client "container1") => (throws Exception)))
+    (fact "raises exception when cant attach the container"
+      (with-fake-http [#"/containers/create" {:status 406}]
+        (create client "container2") => (throws Exception)))
+    (fact "raises exception when catches server error"
+      (with-fake-http [#"/containers/create" {:status 500}]
+        (create client "container3") => (throws Exception)))
+    (fact "raises exception when catches unspecified error"
+      (with-fake-http [#"/containers/create" {:status 418}]
+        (create client "container4") => (throws Exception)))))
+
+(facts "show container's information"
+  (let [client (make-client default-host)
+        resp1 {:Id "abc123", :State {:Running false}}]
+    (fact "shows correct containers information"
+      (with-fake-http [#"/containers/abc123/json" (generate-string resp1)]
+        (inspect client "abc123") => resp1))
+    (fact "raises exception when base image doesnt exist"
+      (with-fake-http [#"/containers/abc123/json" {:status 404}]
+        (inspect client "abc123") => (throws Exception)))
+    (fact "raises exception when docker's agent has internal exception"
+      (with-fake-http [#"/containers/abc123/json" {:status 500}]
+        (inspect client "abc123") => (throws Exception)))
+    (fact "raises exception when server responses with unspecified status code"
+      (with-fake-http [#"/containers/abc123/json" {:status 418}]
+        (inspect client "abc123") => (throws Exception)))))
+
+(facts "show top processes"
+  (let [client (make-client default-host)
+        resp1 {:Titles ["USER", "PID"]
+               :Process [["root", "101"]]}]
+    (fact "shows a correct information of the container"
+      (with-fake-http [#"/containers/abc123/top" (generate-string resp1)]
+        (top client "abc123") => resp1))
+    (fact "raises exception when the `id` doesnt exists"
+      (with-fake-http [#"/containers/abc123/top" {:status 404}]
+        (top client "abc123") => (throws Exception)))
+    (fact "raises exception when server encounter internal error"
+      (with-fake-http [#"/containers/abc123/top" {:status 500}]
+        (top client "abc123") => (throws Exception)))
+    (fact "raises exception when encounters unspecified error"
+      (with-fake-http [#"/containers/abc123/top" {:status 418}]
+        (top client "abc123") => (throws Exception)))))
+
+(facts "show changes on container"
+  (let [client (make-client default-host)
+        change1 {:Path "/dev" :Kind 0}
+        change2 {:Path "/dev/kmesg" :Kind 1}]
+    (fact "shows a correct response"
+      (with-fake-http [#"/containers/abc123/changes" (generate-string [change1 change2])]
+        (let [resp (changes client "abc123")]
+          (first resp) => change1
+          (second resp) => change2)))
+    (fact "raises exception when the `id` doesnt exist"
+      (with-fake-http [#"/containers/abc124/changes" {:status 404}]
+        (changes client "abc124") => (throws Exception)))
+    (fact "raises exception when encounters internal server error"
+      (with-fake-http [#"/containers/abc124/changes" {:status 500}]
+        (changes client "abc124") => (throws Exception)))
+    (fact "raises exception when encounters unspecified error"
+      (with-fake-http [#"/containers/abc124/changes" {:status 418}]
+        (changes client "abc124") => (throws Exception)))))
+
+(facts "exports a container")
+
